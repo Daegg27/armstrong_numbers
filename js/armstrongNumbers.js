945ef9075e0ad1bbf4905550e48eaaddfded0534
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-26 17:59:53 -0700

    finished

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,35 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
+//exports.
+
+findArmstrongNumbers = function(arr) {
+
+let answer = [];
+let sumOfArr = 0;
+
+// let splitNum = arr.toString().split('').map(elem => (elem));
+
+for (let i = 0; i <= arr[0]; i++ )
+{
+    if (i < 10)
+    {
+        answer.push(i);
+    }else{
+        let splitNum = i.toString().split('').map(elem => (elem));
+        let initialSum = 0;
+    for (let j = 0; j < splitNum.length; j++)
+    {
+        initialSum += splitNum[j] ** splitNum.length;
+        
+    }
+    sumOfArr = initialSum;
+    if (sumOfArr == parseInt(splitNum.join(""))){
+        answer.push(sumOfArr);
+    }
+    }
+}return answer
 
 };
+console.log(findArmstrongNumbers([8]))
+console.log(findArmstrongNumbers([99]))
+console.log(findArmstrongNumbers([999]))
